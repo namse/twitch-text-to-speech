e88a9a05152875753d2ed0ef04a3c6789e6cf6a3
skatpgusskat <skatpgusskat@naver.com>
2018-10-12 23:06:00 +0900

    Add english Brian

--- public/textToSpeech.js
+++ public/textToSpeech.js
@@ -3,12 +3,17 @@ const AWS = require('aws-sdk');
 AWS.config.region = 'ap-northeast-2';
 const polly = new AWS.Polly();
 
+function isEnglish(text) {
+  return /^[\x00-\x7F]*$/.test(text);
+}
+
 async function textToSpeech(text) {
+
   const params = {
     OutputFormat: 'mp3',
     Text: text,
     TextType: 'text',
-    VoiceId: 'Seoyeon',
+    VoiceId: isEnglish(text) ? 'Brian' : 'Seoyeon',
   };
   const {
     AudioStream: audioStream,
